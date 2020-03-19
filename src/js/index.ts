803c1701a12ff2f871640d36816dcc6d4e52e9d5
jngermon <jn.germon@gmail.com>
2020-03-19 09:12:52 +0100

    Add forgotten export

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,11 +1,12 @@
 import Loader from './loader'
 import LoadingScreen from './loading-screen'
-import { LoaderRequest } from './loader-request'
+import { LoaderRequest, LoaderRequestInformations } from './loader-request'
 import { SimpleLoader, RequestSettings as SimpleResourceRequestSettings } from './simple-loader'
 
 export {
     Loader,
     LoaderRequest,
+    LoaderRequestInformations,
     LoadingScreen,
     SimpleLoader,
     SimpleResourceRequestSettings
