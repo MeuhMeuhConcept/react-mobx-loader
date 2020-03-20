d19c629ccf403aa48cb941ebd1ecede8f5ea902c
jngermon <jn.germon@gmail.com>
2020-03-20 14:37:42 +0100

    Add forgotten export

--- build/index.js
+++ build/index.js
@@ -1,6 +1,8 @@
 import Loader from './loader';
 import LoadingScreen from './loading-screen';
 import * as LoaderRequest from './loader-request';
+import { CustomLoader } from './custom-loader';
 import { JsonLoader } from './json-loader';
+import { PrivateLoader } from './private-loader';
 import { SimpleLoader } from './simple-loader';
-export { JsonLoader, Loader, LoaderRequest, LoadingScreen, SimpleLoader };
+export { CustomLoader, JsonLoader, Loader, LoaderRequest, LoadingScreen, PrivateLoader, SimpleLoader };
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,13 +1,17 @@
 import Loader from './loader'
 import LoadingScreen from './loading-screen'
 import * as LoaderRequest from './loader-request'
+import { CustomLoader } from './custom-loader'
 import { JsonLoader } from './json-loader'
+import { PrivateLoader } from './private-loader'
 import { SimpleLoader } from './simple-loader'
 
 export {
+    CustomLoader,
     JsonLoader,
     Loader,
     LoaderRequest,
     LoadingScreen,
+    PrivateLoader,
     SimpleLoader
 }
